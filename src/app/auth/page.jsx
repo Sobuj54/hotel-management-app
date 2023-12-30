08438821bc99b37e15273b4623f21983d9b77b55
soubj <sobujahmed124@gmail.com>
2023-12-30 22:40:39 +0600

    retrieving form data from signup form

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -1,9 +1,38 @@
+"use client";
+
+import { useState } from "react";
 import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
 
+const defaultFormData = {
+  name: "",
+  email: "",
+  password: "",
+};
+
 const Auth = () => {
+  const [formData, setFormData] = useState(defaultFormData);
+
   const inputStyles =
     "border border-gray-300 sm:text-sm rounded-lg block w-full p-2.5 focus:outline-none";
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      console.log(formData);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setFormData(defaultFormData);
+    }
+  };
+
   return (
     <section className="container mx-auto">
       <div className="p-6 space-y-4 md:space-y-6 sm:p-8 w-80 md:w-[70%] mx-auto">
@@ -19,31 +48,34 @@ const Auth = () => {
             <FcGoogle className="ml-3 text-4xl cursor-pointer" />
           </span>
         </div>
-        <form className="space-y-4 md:space-y-6">
+        <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6">
           <input
             type="text"
             name="name"
-            id="name"
             placeholder="Your Name"
             required
             className={inputStyles}
+            value={formData.name}
+            onChange={handleChange}
           />
           <input
             type="email"
             name="email"
-            id="email"
             placeholder="Your Email"
             required
             className={inputStyles}
+            value={formData.email}
+            onChange={handleChange}
           />
           <input
             type="password"
             name="password"
-            id="password"
             placeholder="Enter password"
             required
             minLength={6}
             className={inputStyles}
+            value={formData.password}
+            onChange={handleChange}
           />
 
           <button
